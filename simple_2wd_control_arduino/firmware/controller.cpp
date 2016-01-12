a3b8a1cb5e69204657b044eaf5102866e8febe32
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-01-12 23:00:55 +0100

    Add motor lib, basic pub/sub ROS demo

--- simple_2wd_control_arduino/firmware/controller.cpp
+++ simple_2wd_control_arduino/firmware/controller.cpp
@@ -1,7 +1,9 @@
 #include <ros.h>
 #include <std_msgs/String.h>
+#include <std_msgs/UInt16.h>
 
 #include <Arduino.h>
+#include <AFMotor.h>
 
 ros::NodeHandle nh;
 
@@ -9,16 +11,35 @@ std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 
 char hello[13] = "hello world!";
+char bla[13] = "bla";
+uint16_t val_;
+
+void servo_cb( const std_msgs::UInt16& cmd_msg){
+  val_ = cmd_msg.data;
+  //servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
+  //digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+}
+
+
+ros::Subscriber<std_msgs::UInt16> sub("servo", servo_cb);
 
 void setup()
 {
+  val_ = 0;
+
   nh.initNode();
   nh.advertise(chatter);
+
+  nh.subscribe(sub);
 }
 
 void loop()
 {
-  str_msg.data = hello;
+  if (val_ < 10)
+    str_msg.data = hello;
+  else
+    str_msg.data = bla;
+  
   chatter.publish( &str_msg );
   nh.spinOnce();
   delay(1000);

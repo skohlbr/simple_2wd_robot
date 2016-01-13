630b34f7047527b0e12935bb38023fee23c4b97d
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-01-13 22:25:43 +0100

    Updates

--- simple_2wd_control_arduino/firmware/controller.cpp
+++ simple_2wd_control_arduino/firmware/controller.cpp
@@ -1,46 +1,63 @@
 #include <ros.h>
 #include <std_msgs/String.h>
-#include <std_msgs/UInt16.h>
+//#include <std_msgs/UInt16.h>
+#include <simple_arduino_control_msgs/DiffDriveCommand.h>
+
+//#include "../ros_lib/simple_arduino_control_msgs/DiffDriveCommand.h"
+
 
 #include <Arduino.h>
 #include <AFMotor.h>
 
 ros::NodeHandle nh;
 
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
+AF_DCMotor left_motor(2, MOTOR12_64KHZ);
+AF_DCMotor right_motor(1, MOTOR12_64KHZ);
 
-char hello[13] = "hello world!";
-char bla[13] = "bla";
-uint16_t val_;
+int led = 13;
 
-void servo_cb( const std_msgs::UInt16& cmd_msg){
-  val_ = cmd_msg.data;
-  //servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
-  //digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+
+unsigned long last_cmd_time;
+
+void command_cb( const simple_arduino_control_msgs::DiffDriveCommand& cmd_msg){
+
+  if (cmd_msg.left_motor_cmd == 0){
+    left_motor.run(RELEASE);
+    left_motor.setSpeed(0);
+  }else if (cmd_msg.left_motor_cmd > 0){
+    left_motor.run(FORWARD);
+    left_motor.setSpeed( cmd_msg.left_motor_cmd*2);
+  }else{
+    left_motor.run(BACKWARD);
+    left_motor.setSpeed(-cmd_msg.left_motor_cmd*2);
+  }
+
+  if (cmd_msg.right_motor_cmd == 0){
+    right_motor.run(RELEASE);
+    right_motor.setSpeed(0);
+  }else if (cmd_msg.right_motor_cmd > 0){
+    right_motor.run(FORWARD);
+    right_motor.setSpeed( cmd_msg.right_motor_cmd*2);
+  }else{
+    right_motor.run(BACKWARD);
+    right_motor.setSpeed(-cmd_msg.right_motor_cmd*2);
+  }
+
 }
 
 
-ros::Subscriber<std_msgs::UInt16> sub("servo", servo_cb);
+ros::Subscriber<simple_arduino_control_msgs::DiffDriveCommand> sub("move_command_raw", command_cb);
 
 void setup()
 {
-  val_ = 0;
 
   nh.initNode();
-  nh.advertise(chatter);
 
   nh.subscribe(sub);
 }
 
 void loop()
 {
-  if (val_ < 10)
-    str_msg.data = hello;
-  else
-    str_msg.data = bla;
-  
-  chatter.publish( &str_msg );
   nh.spinOnce();
-  delay(1000);
+  delay(1);
 }
---
+++ simple_2wd_control_arduino/scripts/upload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This only works when in the workspace root, has to be made more robust
+catkin_make simple_2wd_control_arduino_firmware_controller-upload

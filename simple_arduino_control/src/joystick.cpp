e420614b280c09a4bd93807e9675da466f339946
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-01-20 11:07:38 +0100

    Updates, start most nodes on robot

--- simple_arduino_control/src/joystick.cpp
+++ simple_arduino_control/src/joystick.cpp
@@ -8,6 +8,8 @@
 namespace simple_joy_control {
 
   ros::Subscriber joyInput;
+  ros::Subscriber twistInput;
+
   ros::Publisher motionCommandOutput;
 
   ros::Publisher twistCommandOutput;
@@ -22,17 +24,14 @@ namespace simple_joy_control {
   double axis_speed_dead_zone;
   double axis_steer_dead_zone;
 
-
-
-  void joyCallback(const sensor_msgs::JoyConstPtr joystick) {
-
-    float speed = joystick->axes[axis_speed];
+  void updateCommand(float speed_in, float angRate_in)
+  {
+    float speed = speed_in;
+    float angRate = speed_in;
 
     if (std::abs(speed) < axis_speed_dead_zone){
      speed = 0.0f;
     }
-
-    float angRate = joystick->axes[axis_steer];
 
     if(std::abs(angRate) < axis_steer_dead_zone){
       angRate = 0.0f;
@@ -42,61 +41,38 @@ namespace simple_joy_control {
 
     //twistCommand.linear.x = speed;
     //twistCommand.angular.z = angRateSpeed;
-    
+
     float mag_left = speed - angRate;
     float mag_right = speed + angRate;
-    
+
     float mag_max = std::max(mag_left, mag_right);
-    
+
     if (mag_max > 1.0f){
       mag_left = mag_left / mag_max;
       mag_right = mag_right / mag_max;
     }
-    
+
     motionCommand.left_motor_cmd = static_cast<int8_t>(mag_left * 127.0);
     motionCommand.right_motor_cmd = static_cast<int8_t>(mag_right * 127.0);
-    
-           
-    //pwm_left = round(self._Max_PWM*mag_left)                      
-    //pwm_right = round(self._Max_PWM*mag_right)
 
-
-    /*
-    if (axis_speed > 0 && (size_t) axis_speed <= joystick->axes.size()) {
-      float value = joystick->axes[axis_speed-1];
-      if (value >= 0.0)
-        motionCommand.speed = value * speedForward;
-      else
-        motionCommand.speed = value * speedBackward;
-
-      if (button_slow > 0 && (size_t) button_slow <= joystick->buttons.size() && joystick->buttons[button_slow-1]) {
-        motionCommand.speed *= slowFactor;
-      }
-    }
-
-    if (axis_steer > 0 && (size_t) axis_steer <= joystick->axes.size()) {
-      motionCommand.steerAngleFront = joystick->axes[axis_steer-1] * steerAngle * M_PI/180.0;
-      motionCommand.steerAngleRear  = motionCommand.steerAngleFront;
-
-      if (button_crab > 0 && (size_t) button_crab <= joystick->buttons.size()) {
-        if (joystick->buttons[button_crab-1]) motionCommand.steerAngleRear = -motionCommand.steerAngleRear;
-      }
-    }
-
-    motionCommandOutput.publish(motionCommand);
-
-    if (axis_cameraPan > 0 && (size_t) axis_cameraPan <= joystick->axes.size()) {
-      cameraPanSpeed = joystick->axes[axis_cameraPan-1] * cameraSpeed * M_PI/180.0;
-    }
-    if (axis_cameraTilt > 0 && (size_t) axis_cameraTilt <= joystick->axes.size()) {
-      cameraTiltSpeed = joystick->axes[axis_cameraTilt-1] * cameraSpeed * M_PI/180.0;
-    }
-    if (button_cameraReset > 0 && (size_t) button_cameraReset <= joystick->buttons.size() && joystick->buttons[button_cameraReset-1]) {
-      cameraPan = 0;
-      cameraTilt = 0;
-    }
-    */
   }
+  
+  void twistCallback(const geometry_msgs::TwistConstPtr twist)
+  {
+    float speed = twist->linear.x;
+    float angRate = twist->angular.z;
+    
+    updateCommand(speed, angRate);
+  }
+
+  void joyCallback(const sensor_msgs::JoyConstPtr joystick) {
+
+    float speed = joystick->axes[axis_speed];
+    float angRate = joystick->axes[axis_steer];
+    
+    updateCommand(speed, angRate);
+  }
+  
 };
 
 
@@ -108,9 +84,10 @@ int main(int argc, char **argv) {
   ros::NodeHandle n;
 
   joyInput = n.subscribe("joy", 10, joyCallback);
+  twistInput = n.subscribe("cmd_vel", 10, twistCallback);
   motionCommandOutput = n.advertise<simple_arduino_control_msgs::DiffDriveCommand>("/move_command_raw", 10, false);
 
-  twistCommandOutput = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10, false);
+  //twistCommandOutput = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10, false);
   //cameraCommandOutput = n.advertise<geometry_msgs::QuaternionStamped>("camera/command", 10, false);
 
   ros::param::param("~axis_speed", simple_joy_control::axis_speed, 0);
